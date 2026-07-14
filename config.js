4e40f14b85e868ff172e7ce08c0116fd8b6a84d5
InfinixPH <infinixph.00@gmail.com>
2026-07-14 14:47:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Google Apps Script Web App URL below.
 // (Deploy > New deployment > Web app > "Anyone" access, then copy the URL it gives you.)
-const API_URL = "https://script.google.com/macros/s/AKfycbyPt_sy-QrQHy51u0z7Pov9Nc-ZasazuVYpK2MXNlAxMUsi0i9lqMcZsmC6xnV_SUiV/execRL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbyPt_sy-QrQHy51u0z7Pov9Nc-ZasazuVYpK2MXNlAxMUsi0i9lqMcZsmC6xnV_SUiV/exec";
